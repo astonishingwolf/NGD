a12116536c8f5bc20b3fae11732b3fa8f96868bc
astonishingwolf <soham_d@me.iitr.ac.in>
2025-03-07 01:12:30 +0530

    Updated code

--- bash_train.sh
+++ bash_train.sh
@@ -1,13 +1,13 @@
-python main.py --config /home/nakul/soham/3d/MonoClothRecon/scripts/configs/dress4D/169/upper_people.yaml
-python main.py --config /home/nakul/soham/3d/MonoClothRecon/scripts/configs/dress4D/169/lower_people.yaml
-python main.py --config /home/nakul/soham/3d/MonoClothRecon/scripts/configs/dress4D/185/upper_people.yaml
-python main.py --config /home/nakul/soham/3d/MonoClothRecon/scripts/configs/dress4D/185/lower_people.yaml
+# python main.py --config /home/nakul/soham/3d/MonoClothRecon/scripts/configs/dress4D/169/upper_people.yaml
+# python main.py --config /home/nakul/soham/3d/MonoClothRecon/scripts/configs/dress4D/169/lower_people.yaml
+# python main.py --config /home/nakul/soham/3d/MonoClothRecon/scripts/configs/dress4D/185/upper_people.yaml
+# python main.py --config /home/nakul/soham/3d/MonoClothRecon/scripts/configs/dress4D/185/lower_people.yaml
 # python main.py --config /home/nakul/soham/3d/MonoClothRecon/scripts/configs/dress4D/187/upper_people.yml
 # python main.py --config /home/nakul/soham/3d/MonoClothRecon/scripts/configs/dress4D/148/upper_people.yml
-# python main.py --config /home/nakul/soham/3d/MonoClothRecon/scripts/configs/dress4D/Demo/upper_people.yml    
-# python main.py --config /home/nakul/soham/3d/MonoClothRecon/scripts/configs/dress4D/Demo/lower_people.yml    
+python main.py --config /home/nakul/soham/3d/MonoClothRecon/scripts/configs/dress4D/Demo/upper_people.yml    
+python main.py --config /home/nakul/soham/3d/MonoClothRecon/scripts/configs/dress4D/Demo/lower_people.yml    
 # python main.py --config /home/nakul/soham/3d/MonoClothRecon/scripts/configs/dress4D/169/upper_people.yaml
-python main.py --config /home/nakul/soham/3d/MonoClothRecon/scripts/configs/people_snapshot/anran_tic/upper.yaml
-python main.py --config /home/nakul/soham/3d/MonoClothRecon/scripts/configs/people_snapshot/anran_tic/lower.yaml
+# python main.py --config /home/nakul/soham/3d/MonoClothRecon/scripts/configs/people_snapshot/anran_tic/upper.yaml
+# python main.py --config /home/nakul/soham/3d/MonoClothRecon/scripts/configs/people_snapshot/anran_tic/lower.yaml
 # python main.py --config /home/nakul/soham/3d/MonoClothRecon/scripts/configs/dress4D/Demo/lower_people.yml
 # python main.py --config /home/nakul/soham/3d/MonoClothRecon/scripts/configs/dress4D/Demo/upper_people.yml
